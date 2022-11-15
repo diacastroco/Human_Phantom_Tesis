edba0f70467222819316fc79b8e8eab0a2c93b28
diacastroco <diacastroco@unal.edu.co>
2022-11-15 10:47:57 -0500

    Archivos human_phantom Geant4 para testear

---
+++ build/CMakeFiles/phantom.dir/DependInfo.cmake
@@ -0,0 +1,117 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/castrensis/Tesis/human_phantom/phantom.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/phantom.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4BasePhantomBuilder.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4BasePhantomBuilder.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4CustomFemaleBuilder.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4CustomFemaleBuilder.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4FemaleBuilder.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4FemaleBuilder.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4HumanPhantomActionInitialization.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4HumanPhantomActionInitialization.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4HumanPhantomAnalysisManager.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4HumanPhantomAnalysisManager.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4HumanPhantomColour.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4HumanPhantomColour.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4HumanPhantomConstruction.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4HumanPhantomConstruction.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4HumanPhantomEventAction.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4HumanPhantomEventAction.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4HumanPhantomHit.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4HumanPhantomHit.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4HumanPhantomMaterial.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4HumanPhantomMaterial.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4HumanPhantomMessenger.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4HumanPhantomMessenger.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4HumanPhantomPhysicsList.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4HumanPhantomPhysicsList.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4HumanPhantomPrimaryGeneratorAction.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4HumanPhantomPrimaryGeneratorAction.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4HumanPhantomRunAction.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4HumanPhantomRunAction.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4HumanPhantomSD.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4HumanPhantomSD.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4HumanPhantomSteppingAction.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4HumanPhantomSteppingAction.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4MIRDBodyFactory.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDBodyFactory.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4MIRDBrain.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDBrain.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4MIRDHead.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDHead.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4MIRDHeart.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDHeart.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4MIRDLeftAdrenal.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDLeftAdrenal.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4MIRDLeftArmBone.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDLeftArmBone.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4MIRDLeftBreast.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDLeftBreast.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4MIRDLeftClavicle.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDLeftClavicle.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4MIRDLeftKidney.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDLeftKidney.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4MIRDLeftLeg.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDLeftLeg.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4MIRDLeftLegBone.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDLeftLegBone.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4MIRDLeftLung.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDLeftLung.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4MIRDLeftOvary.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDLeftOvary.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4MIRDLeftScapula.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDLeftScapula.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4MIRDLeftTeste.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDLeftTeste.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4MIRDLiver.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDLiver.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4MIRDLowerLargeIntestine.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDLowerLargeIntestine.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4MIRDMaleGenitalia.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDMaleGenitalia.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4MIRDMiddleLowerSpine.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDMiddleLowerSpine.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4MIRDPancreas.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDPancreas.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4MIRDPelvis.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDPelvis.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4MIRDRibCage.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDRibCage.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4MIRDRightAdrenal.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDRightAdrenal.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4MIRDRightArmBone.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDRightArmBone.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4MIRDRightBreast.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDRightBreast.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4MIRDRightClavicle.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDRightClavicle.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4MIRDRightKidney.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDRightKidney.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4MIRDRightLeg.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDRightLeg.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4MIRDRightLegBone.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDRightLegBone.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4MIRDRightLung.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDRightLung.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4MIRDRightOvary.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDRightOvary.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4MIRDRightScapula.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDRightScapula.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4MIRDRightTeste.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDRightTeste.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4MIRDSkull.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDSkull.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4MIRDSmallIntestine.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDSmallIntestine.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4MIRDSpleen.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDSpleen.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4MIRDStomach.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDStomach.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4MIRDThymus.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDThymus.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4MIRDThyroid.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDThyroid.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4MIRDTrunk.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDTrunk.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4MIRDUpperLargeIntestine.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDUpperLargeIntestine.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4MIRDUpperSpine.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDUpperSpine.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4MIRDUrinaryBladder.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDUrinaryBladder.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4MIRDUterus.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDUterus.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4MaleBuilder.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4MaleBuilder.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4ORNLFemaleBodyFactory.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4ORNLFemaleBodyFactory.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4ORNLMaleBodyFactory.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4ORNLMaleBodyFactory.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4PhantomBuilder.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4PhantomBuilder.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4PhantomHeadBuilder.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4PhantomHeadBuilder.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4VBodyFactory.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4VBodyFactory.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4VOrgan.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4VOrgan.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4VoxelBreastFactory.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4VoxelBreastFactory.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4VoxelLeftBreast.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4VoxelLeftBreast.cc.o"
+  "/home/castrensis/Tesis/human_phantom/src/G4VoxelRightBreast.cc" "/home/castrensis/Tesis/human_phantom/build/CMakeFiles/phantom.dir/src/G4VoxelRightBreast.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4INTY_USE_QT"
+  "G4INTY_USE_XT"
+  "G4LIB_BUILD_DLL"
+  "G4UI_USE_QT"
+  "G4UI_USE_TCSH"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLQT"
+  "G4VIS_USE_RAYTRACERX"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_OPENGL_LIB"
+  "QT_PRINTSUPPORT_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/home/castrensis/geant4/geant4.10.07.p03-install/include/Geant4"
+  "/usr/include/x86_64-linux-gnu/qt5"
+  "/usr/include/x86_64-linux-gnu/qt5/QtCore"
+  "/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++"
+  "/usr/include/x86_64-linux-gnu/qt5/QtGui"
+  "/usr/include/x86_64-linux-gnu/qt5/QtWidgets"
+  "/usr/include/x86_64-linux-gnu/qt5/QtOpenGL"
+  "/usr/include/x86_64-linux-gnu/qt5/QtPrintSupport"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
